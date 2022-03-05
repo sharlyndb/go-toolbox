ddca69d4812c247f1d79a428b87291808903e855
coseman <904497924@qq.com>
2022-03-06 00:53:09 +0800

    新增请求封装

---
+++ request/ids.go
@@ -0,0 +1,39 @@
+/**
+ * @Time: 2022/3/6 00:46
+ * @Author: yt.yin
+ */
+
+package request
+
+// ID 接收单个请求体json格式的id
+type ID struct {
+
+	/** 主键id */
+	ID string `json:"id"            form:"id"`
+
+	/** 表名 */
+	TableName string `json:"tableName"     form:"tableName"`
+}
+
+// NumIds 接收数字型的id数组
+type NumIds struct {
+
+	/** 主键id集合 */
+	Ids []int `json:"ids"           form:"ids"`
+
+	/** 表名 */
+	TableName string `json:"tableName"     form:"tableName"`
+}
+
+// StrIds 字符串型的id数组
+type StrIds struct {
+	Ids []string `json:"ids"           form:"ids"`
+
+	/** 表名 */
+	TableName string `json:"tableName"     form:"tableName"`
+}
+
+// TableReq 接收单个请求体json格式的表名
+type TableReq struct {
+	TableName string `json:"tableName"     form:"tableName"`
+}
