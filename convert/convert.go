7029512777e546154c90cf0b32694337ba8f5e9b
coseman <904497924@qq.com>
2022-03-10 16:35:14 +0800

    新增了开发常用的转换工具

--- convert/convert.go
+++ convert/convert.go
@@ -286,16 +286,28 @@ func AsString(src interface{}) string {
 	switch v := src.(type) {
 	case string:
 		return v
-	case byte:
-		return string([]byte{v})
 	case []byte:
 		return string(v)
 	case int:
 		return strconv.Itoa(v)
+	case int8:
+		return strconv.Itoa(int(v))
+	case int16:
+		return strconv.Itoa(int(v))
 	case int32:
 		return strconv.FormatInt(int64(v), 10)
 	case int64:
 		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
 	case float32:
 		return strconv.FormatFloat(float64(v), 'f', -1, 64)
 	case float64:
